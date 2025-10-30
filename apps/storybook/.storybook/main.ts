3038b08571c44cd4ae477a929d09b0ad78ad4311
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-30 12:01:20 +0900

    [Project] Setting: 스토리북이 참조할 UI 경로 재설정

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,15 +1,11 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-links",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -17,15 +13,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      resolve: {
-        alias: {
-          "@grapicar-studio/design-system": "../../packages/ui/src/index.ts",
-        },
-      },
-    });
   },
 };
 
