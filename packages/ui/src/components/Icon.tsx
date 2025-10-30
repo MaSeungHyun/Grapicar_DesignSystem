3bb9cfcd8b56c2077b97682c57efbbbef06c7aba
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-30 16:54:23 +0900

    [Icon] Setting: Lucide 라이브러리를 사용한 아이콘 컴포넌트 구현

---
+++ packages/ui/src/components/Icon.tsx
@@ -0,0 +1,31 @@
+import { icons, type LucideProps } from "lucide-react";
+import { cn } from "../utils/style";
+
+type IconProps = LucideProps & {
+  icon: keyof typeof icons;
+  className?: string;
+  fill?: string;
+  size?: number;
+  onClick?: () => void;
+};
+
+export default function Icon({
+  className,
+  icon,
+  fill = "transparent",
+  size = 12,
+  onClick,
+  ...props
+}: IconProps): React.ReactNode {
+  const Icon = icons[icon as keyof typeof icons];
+
+  return (
+    <Icon
+      className={cn("stroke-white", className)}
+      fill={fill}
+      size={size}
+      onClick={onClick}
+      {...props}
+    />
+  );
+}
