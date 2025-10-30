a84b9ee9f27d5834ba20e7e084fee85277de0f95
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-30 13:38:07 +0900

    [Storybook] feat: 스토리북 기본 배경 어두운 색상 설정

--- apps/storybook/stories/Button.stories.tsx
+++ apps/storybook/stories/Button.stories.tsx
@@ -9,30 +9,28 @@ const meta = {
   },
   tags: ["autodocs"],
   argTypes: {
-    variant: {
-      control: "select",
-      options: ["primary", "secondary", "outline"],
-    },
-    size: {
-      control: "select",
-      options: ["small", "medium", "large"],
-    },
+    // variant: {
+    //   control: "select",
+    //   options: ["primary", "secondary", "outline"],
+    // },
+    // size: {
+    //   control: "select",
+    //   options: ["small", "medium", "large"],
+    // },
   },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const DEFAULT: Story = {
+export const Deafult: Story = {
   args: {
-    variant: "primary",
-    children: "Primary Button",
+    children: "Default Button",
   },
 };
 
-export const DISABLED: Story = {
+export const Disabled: Story = {
   args: {
-    variant: "secondary",
-    children: "Secondary Button",
+    children: "Disabled Button",
   },
 };
