754a34b34a1980c2075d49c232ed864ffb99f7b3
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-31 10:27:35 +0900

    [Root] feat: 빠른 컴포넌트 개발을 위하여 컴포넌트 개발 템플릿 작성 해당 Template 폴더를 복사 붙여넣기하여 개발을 진행하면 됩니다.

---
+++ packages/ui/src/components/_Template/index.ts
@@ -0,0 +1,4 @@
+export { Template } from "./index.tsx";
+export type { TemplateProps } from "./index.tsx";
+
+// ⭐ src root 폴더 하위 index.ts에서 한 번 더 export 해줘야 함
---
+++ packages/ui/src/components/_Template/index.tsx
@@ -0,0 +1,5 @@
+export type TemplateProps = {};
+
+export const Template = ({ ...props }: TemplateProps) => {
+  return <div>{/* Component 내용 작성 */}</div>;
+};
