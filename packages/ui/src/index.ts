0fa2ae1b1426243a4ad36d6713b2c5a9ad7da336
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-31 09:38:25 +0900

    [Project] chore: 모듈 추출 방식 변경을 통한 import 변경

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,5 +3,7 @@ import "./styles.css";
 
 // Export all components here
 export { Button } from "./components/Button";
+export { Icon } from "./components/Icon";
 export { Input } from "./components/Input";
 export { SearchBar } from "./components/SearchBar";
+export { Toggle } from "./components/Toggle";
