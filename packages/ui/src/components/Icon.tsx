4f6e165ab8feeeb4d1a06ba09ce47259a394f015
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-31 14:33:26 +0900

    [Icon] fix: fill 값을 className으로 받아 아이콘 색상 자율성 향상 및 클릭 이벤트 제거

--- packages/ui/src/components/Icon.tsx
+++ packages/ui/src/components/Icon.tsx
@@ -1,31 +1,19 @@
 import { icons, type LucideProps } from "lucide-react";
 import { cn } from "../utils/style";
 
-type IconProps = LucideProps & {
+export type IconProps = LucideProps & {
   icon: keyof typeof icons;
   className?: string;
-  fill?: string;
   size?: number;
-  onClick?: () => void;
 };
 
 export const Icon = ({
   className = "",
   icon = "Box",
-  fill = "transparent",
   size = 12,
-  onClick = () => {},
   ...props
 }: IconProps): React.ReactNode => {
   const Icon = icons[icon as keyof typeof icons];
 
-  return (
-    <Icon
-      className={cn("stroke-white", className)}
-      fill={fill}
-      size={size}
-      onClick={onClick}
-      {...props}
-    />
-  );
+  return <Icon className={cn("stroke-white", className)} size={size} {...props} />;
 };
