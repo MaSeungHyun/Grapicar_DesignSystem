96cc22827596202015580441cd591b3a46ad44db
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-10-31 14:31:28 +0900

    [Storybook] feat: args 타입이 아닌 사용자 상호작용을 통한 컴포넌트 테스트 적용

--- apps/storybook/stories/Toggle.stories.tsx
+++ apps/storybook/stories/Toggle.stories.tsx
@@ -1,15 +1,14 @@
 import { Icon, Toggle } from "@grapicar-studio/design-system";
+import { useState } from "@storybook/preview-api";
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
+import * as React from "react";
 
 const meta = {
   title: "Components/Toggle",
   component: Toggle,
   parameters: {
     layout: "centered",
-    backgrounds: {
-      default: "light",
-    },
+
     docs: {
       description: {
         component: "사용자 검색을 위한 기본 SearchBar 컴포넌트로, Icon이 추가된 Input입니다.",
@@ -17,52 +16,39 @@ const meta = {
     },
   },
   tags: ["autodocs"],
-  argTypes: {
-    defaultPressed: {
-      control: "boolean",
-      description: "기본 체크 상태",
-      table: {
-        type: { summary: "boolean" },
-        defaultValue: { summary: "false" },
-      },
-    },
-    shape: {
-      control: "select",
-      options: ["square", "circle"],
-      description: "토글 모양",
-      table: {
-        type: { summary: "square | circle" },
-        defaultValue: { summary: "square" },
-      },
-    },
-    pressed: {
-      control: "boolean",
-      description: "체크 상태",
-      table: {
-        type: { summary: "boolean" },
-        defaultValue: { summary: "false" },
-      },
-    },
-    onPressedChange: {
-      control: undefined,
-      description: "체크 상태 변경 이벤트 핸들러",
-      table: {
-        type: { summary: "React.Dispatch<React.SetStateAction<boolean>>" },
-      },
-    },
-  },
 } satisfies Meta<typeof Toggle>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    children: <Icon icon="MousePointer2" className="fill-white" size={16} onClick={() => {}} />,
-    pressed: false,
-    shape: "square",
-    onPressedChange: (value: boolean) => {
-      console.log(value);
-    },
+type ToggleStory = StoryObj<typeof Toggle>;
+
+export const Default: ToggleStory = {
+  render: (args) => {
+    const [pressed, setPressed] = useState(args.pressed ?? false);
+
+    const handlePressedChange = (value: boolean) => {
+      setPressed(value);
+    };
+    return (
+      <Toggle {...args} pressed={pressed} onPressedChange={handlePressedChange}>
+        <Icon icon="MousePointer2" size={12} className="fill-white" />
+      </Toggle>
+    ) as any;
+  },
+};
+
+export const Circle: ToggleStory = {
+  render: (args) => {
+    const [pressed, setPressed] = useState(args.pressed ?? false);
+
+    const handlePressedChange = (value: boolean) => {
+      setPressed(value);
+    };
+
+    return (
+      <Toggle {...args} shape="circle" pressed={pressed} onPressedChange={handlePressedChange}>
+        <Icon icon="MousePointer2" size={12} className="fill-white" />
+      </Toggle>
+    ) as any;
   },
 };
