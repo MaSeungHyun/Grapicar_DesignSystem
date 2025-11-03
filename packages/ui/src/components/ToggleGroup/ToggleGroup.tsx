6c408a58fe540f3b040ddf0df014c619fcbd7672
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-11-03 09:52:46 +0900

    [ToggleGroup] feat: ToggleGroup 공통 컴포넌트 구현

---
+++ packages/ui/src/components/ToggleGroup/ToggleGroup.tsx
@@ -0,0 +1,49 @@
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
+import * as React from "react";
+
+import { cn } from "../../utils/style";
+
+const ToggleGroupRoot = ({
+  className,
+  children,
+  orientation = "horizontal",
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root>) => {
+  return (
+    <ToggleGroupPrimitive.Root
+      className={cn(
+        "flex items-center justify-center data-[orientation=vertical]:flex-col",
+        orientation === "vertical" && "flex-col",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </ToggleGroupPrimitive.Root>
+  );
+};
+
+const ToggleGroupItem = ({
+  className = "",
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item>) => {
+  return (
+    <ToggleGroupPrimitive.Item
+      className={cn(
+        "bg-black-600 group relative flex min-h-5 min-w-5 items-center justify-center text-white hover:bg-gray-600/90",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </ToggleGroupPrimitive.Item>
+  );
+};
+
+ToggleGroupItem.displayName = ToggleGroupPrimitive.Item.displayName;
+
+export const ToggleGroup = Object.assign(ToggleGroupRoot, {
+  Root: ToggleGroupRoot,
+  Item: ToggleGroupItem,
+});
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -7,3 +7,4 @@ export { Icon } from "./components/Icon";
 export { Input } from "./components/Input";
 export { SearchBar } from "./components/SearchBar";
 export { Toggle } from "./components/Toggle";
+export { ToggleGroup } from "./components/ToggleGroup";
