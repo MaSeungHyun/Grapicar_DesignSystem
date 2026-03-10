cfc919f2036efe1f47e488039dd9bc2eac69e8bb
MaSeungHyun <seunghyun.ma@grapicar.com>
2026-03-10 17:49:45 +0900

    [TreeView] bug: 불필요한 상수 변경

--- packages/ui/src/components/TreeView/TreeView.tsx
+++ packages/ui/src/components/TreeView/TreeView.tsx
@@ -10,7 +10,6 @@ export function TreeView({
   tree: initialTree,
   className,
   headerTitle = "Layers",
-  maxHeight = "max-h-80",
 }: TreeViewProps): React.ReactElement {
   const [tree, setTree] = useState(initialTree);
   const [collapsed, setCollapsed] = useState<Set<string | number>>(new Set());
@@ -67,7 +66,7 @@ export function TreeView({
         </div>
 
         <div className="min-h-[1px] w-full" />
-        <div className={cn("relative flex h-full flex-col overflow-y-auto")}>
+        <div className={cn("relative flex h-full flex-col overflow-y-auto", maxHeight)}>
           <div className="bg-black-600 absolute left-px top-px flex h-full w-[36px] flex-1 flex-col" />
           <div className="relative flex h-full flex-col px-px">
             {rows.map((row) => (
